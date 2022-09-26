5a463d32d15c07835fca83c43dd5d99ca835a775
Iskandar Khudzhamkulov <ikhudzhamkul_c@ngs.org>
2022-09-27 01:17:06 +0600

    Fixed functionality.

--- home_task_02.py
+++ home_task_02.py
@@ -14,15 +14,13 @@ Home Task 02 - Collections
 """
 
 
-
 import random
 import string
-from collections import OrderedDict
 
 # 1.
 # creating string of lowercase letters as a source for dict keys:
 letters = string.ascii_lowercase
-# creating variable that will describe lengths of list and dicts:
+# creating variable that will describe lengths of list:
 rnd_len = random.randint(2, 10)
 # creating list od dicts using list and dict comprehensions:
 lst = [{random.choice(letters): random.randint(0, 100) for _ in range(rnd_len)} for _ in range(rnd_len)]
@@ -32,14 +30,23 @@ lst = [{random.choice(letters): random.randint(0, 100) for _ in range(rnd_len)} 
 # Creating empty dict that will contain key-value pairs with
 # max value, according to the task:
 common_dict = {}
+
 # Creating empty dict that will contain key-value pairs with
 # value representing number of the dict with max value:
-positions = {}
+position = {}
+
+# Creating empty dict that will contain key-value pairs that
+# then will be used to rename keys in common dict:
+names = {}
 
 # Let's first iterate through list and then iterate through each dict.
 # For each key-value pair we will seek for same key in our common_dict.
 # If key is present in common_dict we will compare value from iterated
-# dict with the value from common_dict.
+# dict with the value from common_dict in order to find max value.
+# Number of the dict with max value for specified key will be stored in
+# 'position' dict.
+# 'names' dict will store new key-names for 'common_dict, only if comparison
+# of values will occur.
 # If key form iterated dict is NOT in the common dict, then new key-value pair
 # will be added to the common_dict.
 
@@ -48,15 +55,19 @@ for i, dct in enumerate(lst, start=1):
         if key in common_dict:
             if value > common_dict[key]:
                 common_dict[key] = value
-                positions[key] = i
+                position[key] = i
+            names[key] = f'{key}_{position[key]}'
+
         else:
             common_dict[key] = value
+            position[key] = i
+
 
 # Now we will rename keys according to the task.
-# If key from "positions" matches with key from "common_dict"
+# If key from "names" matches with key from "common_dict"
 # then new kay-value pair with appropriate key naming will be created and
 # old key-value pair will be removed by using pop() method.
-for key, value in positions.items():
-    common_dict[f'{key}_{value}'] = common_dict.pop(key)
+for key, value in names.items():
+    common_dict[f'{names[key]}'] = common_dict.pop(key)
 
 print(dict(sorted(common_dict.items())))
